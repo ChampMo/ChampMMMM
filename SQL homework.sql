741dfaa6f515f5006498b8227e2a3323441fd471
Monthol <97272854+ChampMo@users.noreply.github.com>
2023-11-29 00:03:17 +0700

    Update SQL homework.sql

--- SQL homework.sql
+++ SQL homework.sql
@@ -85,7 +85,75 @@ GROUP BY f.film_id, f.title, c.name
 ORDER BY f.film_id;
 
 
+-- unit 10
+use sakila;
 
+-- 1
+select name ,
+	case 
+		when name in ("English","Italian","French","German") then "latin1"
+        when name in ("Japanese","Mandarin") then "utf8"
+        else "Unknow"
+	end character_set
+from language;
+    
+-- 2
+select sum(
+	case
+		when rating = "G" then 1
+        else 0
+	end
+) G_Count ,
+sum(
+	case
+		when rating = "PG" then 1
+        else 0
+	end
+) PG_Count ,
+sum(
+	case
+		when rating = "PG-13" then 1
+        else 0
+	end
+) PG13_Count , 
+sum(
+	case
+		when rating = "R" then 1
+        else 0
+	end
+) R_Count ,
+sum(
+	case
+		when rating = "NC-17" then 1
+        else 0
+	end
+) NC17_Count
+from film;
+
+-- 3
+select table_name , index_name 
+from information_schema.statistics
+where index_schema = "sakila" 
+order by table_name;
+
+-- 4
+select 
+    concat(
+        'ALTER TABLE ', 
+        table_name, 
+        ' ADD INDEX ', 
+        index_name, 
+        ' (', 
+        group_concat(DISTINCT COLUMN_NAME ORDER BY SEQ_IN_INDEX), 
+        ');'
+    ) as CREATE_INDEX_QUERY
+from
+    INFORMATION_SCHEMA.STATISTICS
+where 
+    index_schema = 'sakila' and
+    table_name = 'customer'
+group by
+	table_name , index_name ; 
 
 
 
